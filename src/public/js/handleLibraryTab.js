c3a4e717d12166ed00aa8f8f9c571d1de0f7108d
HoangNgo1310 <Ngotuanhoang1310@gmail.com>
2023-08-22 21:54:40 +0700

    push again

--- src/public/js/handleLibraryTab.js
+++ src/public/js/handleLibraryTab.js
@@ -2,6 +2,6 @@ function goToDiscover(e,target) {
     e.preventDefault();
     window.history.pushState({}, "", target);
     navigateToPage('discoverPage.php', ".container-content");
-
-    
 }
+
+
